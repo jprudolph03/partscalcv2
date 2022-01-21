c00846f5abb12da85c6df9226b3851c67383c9ef
jprudolph03 <jprudolph03@gmail.com>
2022-01-20 20:00:11 -0500

    added some razzle dazzle

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "./App.css";
 
 function App() {
   const [coilTotalKG, setCoilTotalKG] = useState("null");
@@ -40,82 +41,82 @@ function App() {
       <h1 className="text-center display-1">Part Loss Calc</h1>
       <div className="container-sm">
         <div className="col">
-          <h2>Inputs:</h2>
-          <label for="basic-url" class="form-label">
+          <h2 className="display-3">Inputs:</h2>
+          <label for="basic-url" className="form-label fs-3">
             <strong>Total Coil Weight / KG</strong>
           </label>
           <div className="input-group mb-3">
             <input
               type="number"
-              class="form-control"
+              className="form-control shadow p-3 mb-5 bg-body rounded"
               placeholder="Total Coil Weight"
               onChange={handleTotal}
             />
           </div>
-          <label for="basic-url" class="form-label">
+          <label for="basic-url" className="form-label fs-3">
             <strong>STD Loss</strong>
           </label>
           <div className="input-group mb-3">
             <input
               type="number"
-              class="form-control"
+              className="form-control shadow p-3 mb-5 bg-body rounded"
               placeholder="STD Loss"
               onChange={handleSTDLoss}
             />
           </div>
-          <label for="basic-url" class="form-label">
+          <label for="basic-url" className="form-label fs-3">
             <strong>Single Part Weight / G</strong>
           </label>
           <div className="input-group mb-3">
             <input
               type="number"
-              class="form-control"
+              className="form-control shadow p-3 mb-5 bg-body rounded"
               placeholder="Single Part Weight"
               onChange={handlePartWeightG}
             />
           </div>
-          <label for="basic-url" class="form-label">
+          <label for="basic-url" className="form-label fs-3">
             <strong>Counter Total</strong>
           </label>
           <div className="input-group mb-3">
             <input
               type="number"
-              class="form-control"
+              className="form-control shadow p-3 mb-5 bg-body rounded"
               placeholder="Counter Total"
               onChange={handleCounterTotal}
             />
           </div>
-          <label for="basic-url" class="form-label">
+          <label for="basic-url" className="form-label fs-3">
             <strong>Scrap Bucket Weight / KG</strong>
           </label>
           <div className="input-group mb-3">
             <input
               type="number"
-              class="form-control"
+              className="form-control shadow p-3 mb-5 bg-body rounded"
               placeholder="Scrap Bucket Weight"
               onChange={handleScrapWeight}
             />
           </div>
         </div>
         <div className="col">
-          <h2>Outputs:</h2>
-          <p>
+          <h2 className="display-3">Outputs:</h2>
+          <p className="fs-5">
             Total Weight After Loss: <strong>{prodWeight}</strong> Kg
           </p>
-          <p>
+          <p className="fs-5">
             Expected Total 100%: <strong>{parseInt(expectedTotal)}</strong>
           </p>
-          <p>
+          <p className="fs-5">
             Expected vs. Counter = <strong>{parseInt(countDiff)}</strong>
           </p>
-          <p>
+          <p className="fs-5">
             Scrap Parts By Weight ={" "}
             <strong>{parseInt(scrapPartsByWeight)}</strong>
           </p>
-          <p>
+          <p className="fs-5">
             Final Part Count = <strong>{parseInt(finalPartCount)}</strong>
           </p>
-          <p>
+          <p className="fs-5">
             Percent Loss = <strong>{percentLoss}</strong>
           </p>
         </div>
